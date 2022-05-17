a7c91271ae4cf7dc70ceb64e9b3f5f1fb41e60d3
tcrms <visual@shiro.wtf>
2022-05-17 03:36:42 -0400

    make the version match with `package.json`

--- utils.js
+++ utils.js
@@ -25,7 +25,7 @@ export async function DiscordRequest(endpoint, options) {
     headers: {
       Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
       'Content-Type': 'application/json; charset=UTF-8',
-      'User-Agent': 'DiscordBot (https://github.com/discord/discord-example-app, 0.0.0)',
+      'User-Agent': 'DiscordBot (https://github.com/discord/discord-example-app, 1.0.0)',
     },
     ...options
   });
