b6e6a7d71e6cb6da6296a7cd6597b1ecaf88cea9
Lars Tijhuis <ljtijhuis@gmail.com>
2023-04-05 17:13:12 -0700

    Some error handling to prevent server crashes on No results.

--- app.js
+++ app.js
@@ -55,22 +55,32 @@ app.post('/interactions', async function (req, res) {
         },
       });
     } else if (name === 'booksearch') {
-      const searchQuery = data.options[0].value;
-      const searchResults = await searchBooks(searchQuery);
-      const books = [];
-      if (searchResults.books) {
-        books.push(...searchResults.books.slice(0, 3).map((book, i) => {
-          return `${i+1}) ${book.title} by ${book.author} can be found at: ${book.url}`;
-        }));
-      }
-      
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `You searched for ${searchQuery}. The top results I got:\n` 
-          + books.join('\n'),
+      try {
+        const searchQuery = data.options[0].value;
+        const searchResults = await searchBooks(searchQuery);
+        const books = [];
+        if (searchResults.books) {
+          books.push(...searchResults.books.slice(0, 3).map((book, i) => {
+            return `${i+1}) ${book.title} by ${book.author} can be found at: ${book.url}`;
+          }));
         }
-      })
+        
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: `You searched for "${searchQuery}". The top results I got:\n` 
+            + (books.length > 0 ? books.join('\n') : 'No results!'),
+          }
+        });
+      } catch (error) {
+        console.error('error', error);
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: 'Ugh, my creator failed and some error happened...',
+          }
+        });
+      }
     }
   }
 });
