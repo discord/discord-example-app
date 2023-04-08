94c45fac5766f4c0dc2baab1be74c8c2e3536abe
Lars Tijhuis <ljtijhuis@gmail.com>
2023-04-07 20:33:32 -0700

    Moved command code to separate files and more restructuring.

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import express from 'express';
 import {
   InteractionType,
   InteractionResponseType,
-  InteractionResponseFlags,
-  MessageComponentTypes,
-  ButtonStyleTypes,
 } from 'discord-interactions';
-import { VerifyDiscordRequest, getRandomEmoji, DiscordRequest } from './utils.js';
-import { getBookData, searchBooks } from './lib/goodreads_search.js';
+
+import { VerifyDiscordRequest } from './lib/utils.js';
+
+import { testCommand } from './lib/commands/test_command.js';
+import { bookSearchCommand } from './lib/commands/booksearch_command.js';
+import { shortlistCommand } from './lib/commands/shortlist_command.js';
+
 
 // Create an express app
 const app = express();
@@ -47,125 +49,12 @@ app.post('/interactions', async function (req, res) {
   if (type === InteractionType.APPLICATION_COMMAND) {
     const { name } = data;
 
-    // "test" command
     if (name === 'test') {
-      // Send a message into the channel where command was triggered from
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          // Fetches a random emoji to send from a helper function
-          content: 'hello world ' + getRandomEmoji(),
-        },
-      });
+      return testCommand(res);
     } else if (name === 'booksearch') {
-      try {
-        const searchQuery = data.options[0].value;
-        const searchResults = await searchBooks(searchQuery);
-        const books = [];
-        if (searchResults.books) {
-          books.push(...searchResults.books.slice(0, 3).map((book, i) => {
-            return `${i+1}) ${book.title} by ${book.author} can be found at: ${book.url}`;
-          }));
-        }
-        
-        return res.send({
-          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: {
-            content: `You searched for "${searchQuery}". The top results I got:\n` 
-            + (books.length > 0 ? books.join('\n') : 'No results!'),
-          }
-        });
-      } catch (error) {
-        console.error('error', error);
-        return res.send({
-          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: {
-            content: 'Ugh, my creator failed and some error happened...',
-          }
-        });
-      }
+      return bookSearchCommand(res, data);
     } else if (name === 'shortlist') {
-      const subCommand = data.options[0].name;
-      if (subCommand === 'list') {
-        const books = bookClubState.shortlist.books.map((book, i) => {
-          return `${i+1}) ${book.title} by ${book.author} (${book.url}).`;
-        });
-
-        const booksList = books.length > 0 
-          ? books.join('\n') 
-          : 'No books in the shortlist yet! Add some with "/shortlist add <Goodreads url>"';
-
-        return res.send({
-          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: {
-            content: 'This is the current shortlist:\n' + booksList,
-          }
-        });
-      } else if (subCommand === 'add') {
-        const url = data.options[0].options[0].value;
-        
-        await res.send({
-          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            data: {
-              content: 'Request to add the book to the shortlist received! Let me find some more data..'
-            }
-        })
-
-        const book = await getBookData(url);
-
-        if (book !== null) {
-          bookClubState.shortlist.books.push({
-            title: book.title,
-            author: book.author,
-            url: book.url,
-          });
-
-          const resultStr = 'The following book was added to the shortlist:\n' + 
-                `${book.title} by ${book.author} (${book.url}).`
-
-          const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/@original`;
-
-          try {
-            // Update ephemeral message
-            await DiscordRequest(endpoint, {
-              method: 'PATCH',
-              body: {
-                content: resultStr,
-                components: [],
-              },
-            });
-          } catch (err) {
-            console.error('Error sending message:', err);
-          }
-        } else {
-          return res.send({
-            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            data: {
-              content: 'Mehhh.. I couldn\'t find the book.',
-            }
-          });
-        }
-      } else if (subCommand === 'remove') {
-        const removeIndex = data.options[0].options[0].value - 1;
-        if (removeIndex >= 0 && removeIndex < bookClubState.shortlist.books.length) {
-          const [book] = bookClubState.shortlist.books.splice(removeIndex, 1);
-          return res.send({
-            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            data: {
-              content: 'Removed this book from the shortlist:\n' + 
-                `${book.title} by ${book.author} (${book.url}).`
-            }
-          });
-        } else {
-          return res.send({
-            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            data: {
-              content: 'Uh, we don\'t have a book with that number on our shortlist!',
-            }
-          });
-        }
-      }
-
+      return shortlistCommand(res, req, data, bookClubState);
     }
   }
 });
@@ -173,3 +62,4 @@ app.post('/interactions', async function (req, res) {
 app.listen(PORT, () => {
   console.log('Listening on port', PORT);
 });
+
