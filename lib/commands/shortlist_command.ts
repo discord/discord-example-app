88de074c4ea384150f9786fa4d62a66df443ae93
Lars Tijhuis <ljtijhuis@gmail.com>
2023-04-09 15:17:05 -0700

    Move command routing to factory pattern.

--- lib/commands/shortlist_command.ts
+++ lib/commands/shortlist_command.ts
@@ -4,88 +4,95 @@ import { Request, Response } from 'express';
 import { DiscordRequest } from '../utils.js';
 import { getBookData } from '../goodreads/goodreads_search.js';
 import { BookClubState } from '../types/book_club_state.js';
+import { ICommand } from "./command_factory.js";
 
-export async function shortlistCommand(req: Request, res: Response, bookClubState: BookClubState) {
-    const { data } = req.body;
-    const subCommand = data.options[0].name;
-    if (subCommand === 'list') {
-        const books = bookClubState.shortlist.books.map((book, i) => {
-            return `${i + 1}) ${book.title} by ${book.author} (${book.url}).`;
-        });
-
-        const booksList = books.length > 0
-            ? books.join('\n')
-            : 'No books in the shortlist yet! Add some with "/shortlist add <Goodreads url>"';
-
-        return res.send({
-            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            data: {
-                content: 'This is the current shortlist:\n' + booksList,
-            }
-        });
-    } else if (subCommand === 'add') {
-        const url = data.options[0].options[0].value;
-
-        await res.send({
-            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            data: {
-                content: 'Request to add the book to the shortlist received! Let me find some more data..'
-            }
-        })
-
-        const book = await getBookData(url);
-
-        if (book !== null) {
-            bookClubState.shortlist.books.push({
-                id: book.id,
-                title: book.title,
-                author: book.author,
-                url: book.url,
+export class ShortlistCommand implements ICommand {
+    async execute(req: Request, res: Response, state: BookClubState): Promise<Response> {
+        const { data } = req.body;
+        const subCommand = data.options[0].name;
+        if (subCommand === 'list') {
+            const books = state.shortlist.books.map((book, i) => {
+                return `${i + 1}) ${book.title} by ${book.author} (${book.url}).`;
             });
 
-            const resultStr = 'The following book was added to the shortlist:\n' +
-                `${book.title} by ${book.author} (${book.url}).`
+            const booksList = books.length > 0
+                ? books.join('\n')
+                : 'No books in the shortlist yet! Add some with "/shortlist add <Goodreads url>"';
 
-            const endpoint = `webhooks/${config.APP_ID}/${req.body.token}/messages/@original`;
+            return res.send({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                    content: 'This is the current shortlist:\n' + booksList,
+                }
+            });
+        } else if (subCommand === 'add') {
+            const url = data.options[0].options[0].value;
 
-            try {
-                // Update ephemeral message
-                await DiscordRequest(endpoint, {
-                    method: 'PATCH',
-                    body: {
-                        content: resultStr,
-                        components: [],
-                    },
+            const initialSend = await res.send({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                    content: 'Request to add the book to the shortlist received! Let me find some more data..'
+                }
+            })
+
+            const book = await getBookData(url);
+
+            if (book !== null) {
+                state.shortlist.books.push({
+                    id: book.id,
+                    title: book.title,
+                    author: book.author,
+                    url: book.url,
                 });
-            } catch (err) {
-                console.error('Error sending message:', err);
+
+                const resultStr = 'The following book was added to the shortlist:\n' +
+                    `${book.title} by ${book.author} (${book.url}).`
+
+                const endpoint = `webhooks/${config.APP_ID}/${req.body.token}/messages/@original`;
+
+                try {
+                    // Update ephemeral message
+                    await DiscordRequest(endpoint, {
+                        method: 'PATCH',
+                        body: {
+                            content: resultStr,
+                            components: [],
+                        },
+                    });
+                } catch (err) {
+                    console.error('Error sending message:', err);
+                } finally {
+                    return initialSend;
+                }
+            } else {
+                return res.send({
+                    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                    data: {
+                        content: 'Mehhh.. I couldn\'t find the book.',
+                    }
+                });
             }
-        } else {
-            return res.send({
-                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                data: {
-                    content: 'Mehhh.. I couldn\'t find the book.',
-                }
-            });
+        } else if (subCommand === 'remove') {
+            const removeIndex = data.options[0].options[0].value - 1;
+            if (removeIndex >= 0 && removeIndex < state.shortlist.books.length) {
+                const [book] = state.shortlist.books.splice(removeIndex, 1);
+                return res.send({
+                    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                    data: {
+                        content: 'Removed this book from the shortlist:\n' +
+                            `${book.title} by ${book.author} (${book.url}).`
+                    }
+                });
+            } else {
+                return res.send({
+                    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                    data: {
+                        content: 'Uh, we don\'t have a book with that number on our shortlist!',
+                    }
+                });
+            }
         }
-    } else if (subCommand === 'remove') {
-        const removeIndex = data.options[0].options[0].value - 1;
-        if (removeIndex >= 0 && removeIndex < bookClubState.shortlist.books.length) {
-            const [book] = bookClubState.shortlist.books.splice(removeIndex, 1);
-            return res.send({
-                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                data: {
-                    content: 'Removed this book from the shortlist:\n' +
-                        `${book.title} by ${book.author} (${book.url}).`
-                }
-            });
-        } else {
-            return res.send({
-                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                data: {
-                    content: 'Uh, we don\'t have a book with that number on our shortlist!',
-                }
-            });
-        }
+
+        throw new Error('Missing subcommand implementation');
     }
 }
--- lib/commands/test_command.ts
+++ lib/commands/test_command.ts
@@ -1,13 +1,17 @@
 import { InteractionResponseType } from 'discord-interactions';
 import { Request, Response } from 'express';
+import { BookClubState } from '../types/book_club_state.js';
 import { getRandomEmoji } from '../utils.js';
+import { ICommand } from './command_factory.js';
 
-export function testCommand(_: Request, res: Response): void {
-  res.send({
-    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-    data: {
-      // Fetches a random emoji to send from a helper function
-      content: 'hello world ' + getRandomEmoji(),
-    },
-  });
+export class TestCommand implements ICommand {
+  async execute(req: Request, res: Response, state: BookClubState): Promise<Response> {
+    return res.send({
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: {
+        // Fetches a random emoji to send from a helper function
+        content: 'hello world ' + getRandomEmoji(),
+      },
+    });
+  }
 }
