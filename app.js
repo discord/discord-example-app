95ab6a4519f5d9f68fc68eeed973ca557735a861
Angela Ung <angelaung@ufl.edu>
2025-02-17 20:48:48 -0500

    test

--- app.js
+++ app.js
@@ -89,7 +89,88 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
     });
   }
 
-
+  if (type === InteractionType.MESSAGE_COMPONENT) {
+    // custom_id set in payload when sending message component
+    const componentId = data.custom_id;
+    
+      if (componentId.startsWith('accept_button_')) {
+        // get the associated game ID
+        const gameId = componentId.replace('accept_button_', '');
+        // Delete message with token in request body
+        const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/${req.body.message.id}`;
+        try {
+          await res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              content: 'What is your object of choice?',
+              // Indicates it'll be an ephemeral message
+              flags: InteractionResponseFlags.EPHEMERAL,
+              components: [
+                {
+                  type: MessageComponentTypes.ACTION_ROW,
+                  components: [
+                    {
+                      type: MessageComponentTypes.STRING_SELECT,
+                      // Append game ID
+                      custom_id: `select_choice_${gameId}`,
+                      options: getShuffledOptions(),
+                    },
+                  ],
+                },
+              ],
+            },
+          });
+          // Delete previous message
+          await DiscordRequest(endpoint, { method: 'DELETE' });
+        } catch (err) {
+          console.error('Error sending message:', err);
+        }
+      } else if (componentId.startsWith('select_choice_')) {
+        // get the associated game ID
+        const gameId = componentId.replace('select_choice_', '');
+    
+        if (activeGames[gameId]) {
+          // Interaction context
+          const context = req.body.context;
+          // Get user ID and object choice for responding user
+          // User ID is in user field for (G)DMs, and member for servers
+          const userId = context === 0 ? req.body.member.user.id : req.body.user.id;
+    
+          // User's object choice
+          const objectName = data.values[0];
+    
+          // Calculate result from helper function
+          const resultStr = getResult(activeGames[gameId], {
+            id: userId,
+            objectName,
+          });
+    
+          // Remove game from storage
+          delete activeGames[gameId];
+          // Update message with token in request body
+          const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/${req.body.message.id}`;
+    
+          try {
+            // Send results
+            await res.send({
+              type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+              data: { content: resultStr },
+            });
+            // Update ephemeral message
+            await DiscordRequest(endpoint, {
+              method: 'PATCH',
+              body: {
+                content: 'Nice choice ' + getRandomEmoji(),
+                components: []
+              }
+            });
+          } catch (err) {
+            console.error('Error sending message:', err);
+          }
+        }
+      }
+    }
+    
     console.error(`unknown command: ${name}`);
     return res.status(400).json({ error: 'unknown command' });
   }
