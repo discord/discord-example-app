cacf43afdd8590a20b1cdb2b9747c7a9c1f05bdd
Florian Obermayer <florian.obermayer@gmail.com>
2025-04-18 06:42:13 +0200

    removed test from docker again (is broken), env verbose++

--- envHelper.js
+++ envHelper.js
@@ -1,4 +1,8 @@
 export function isTestEnvironment() {
     return process.env.NODE_ENV === 'test' ||
       process.env.MOCHA_RUNNING === 'true';
-  }
+}
+
+export function listAll() {
+  return Object.entries(process.env).map(([key, value]) => `${key}=${value}`);
+}
